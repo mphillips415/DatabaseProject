a9d2226fd1144cd7396f3a52b7752aefb4049a17
mphillips415 <mphillips4@babson.edu>
2016-10-25 12:12:36 -0400

    Project Updated 10/25
    Updated dbcreation and dbqueries

--- BabsonComputers_dbqueries.sql
+++ BabsonComputers_dbqueries.sql
@@ -1,4 +1,9 @@
-﻿Use BabsonComputers
+﻿/*Babson Computers */
+
+/*Eli Iaslovits, Kevin Mitchell, Michael Phillips */
+
+
+Use BabsonComputers
 
 --Select Queries --
 
@@ -35,15 +40,15 @@ Select C.FirstName, C.LastName, SUM(O.TotalDue) as Amount_Owed
 
 -- Query using Left Outer Join --
 
-Select C.FirstName, C.LastName, SUM(O.TotalDue) as Amount_Owed
+Select C.FirstName, C.LastName, C.EmailAddress, SUM(O.TotalDue) as Amount_Owed
  From Customer as C
  Left Outer Join OrderHeader as O
  ON C.CustomerID = O.CustomerID
- Group By C.FirstName, C.LastName
+ Group By C.FirstName, C.LastName, C.EmailAddress
  Order By Amount_Owed DESC
 
- --  Left outer join is necessary to show all the customers and what they owe 
- -- but it also shows which customers have not ordered and we could possibly extend a promotion to them
+ /* Left outer join is necessary to show all the customers and what they owe 
+ but it also shows which customers have not ordered and we could possibly extend a promotion to them */
 
  Select AVG(TotalDue)
   From OrderHeader
